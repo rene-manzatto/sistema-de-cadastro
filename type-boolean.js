51f988eec29dfbd451e1baeb2d42e2594d8b4fa9
rene-manzatto <139180722+rene-manzatto@users.noreply.github.com>
2023-07-17 17:10:50 +0000

    Exemplos aula 1

---
+++ type-boolean.js
@@ -0,0 +1,13 @@
+// boolean
+// true -> Verdadeiro
+// false -> Falso
+
+const primeiroNumero = 5
+const segundoNumero = 5
+
+// console.log(primeiroNumero === segundoNumero)
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2)
